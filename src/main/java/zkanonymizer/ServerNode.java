09a9c110a911c18c0fa182ecb830a5798a40407d
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 22:10:10 +0300

    Scripted auto-commit on change (2021-12-17 22:10:10) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -56,7 +56,7 @@ public class ServerNode extends AllDirectives {
         ServerNode instance = new ServerNode();
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow;
         routeFlow = instance.createRoute(system).flow(system, materializer);
-
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", port),
                 materializer
