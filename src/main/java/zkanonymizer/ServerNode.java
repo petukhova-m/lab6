d5cd794b82c72f38fbb4144db16718cd8838debd
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 20:53:45 +0300

    Scripted auto-commit on change (2021-12-17 20:53:45) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -83,7 +83,7 @@ public class ServerNode extends AllDirectives {
                         return
                                 completeWithFuture(
                                         http.singleRequest(
-
+                                                HttpRequest.create(
                                                     String.format("http://localhost:%d/?url=%s&count=%d",
                                                             Integer.parseInt(
                                                                     (String) Patterns
