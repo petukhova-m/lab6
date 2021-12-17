d30db094f80bae430ef53eb3988e10c672c46099
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 22:05:57 +0300

    Scripted auto-commit on change (2021-12-17 22:05:57) by gitwatch.sh

--- src/main/java/zkanonymizer/ConfigStorageActor.java
+++ src/main/java/zkanonymizer/ConfigStorageActor.java
@@ -8,7 +8,7 @@ import akka.actor.AbstractActor;
 public class ConfigStorageActor extends AbstractActor {
     private ArrayList<String> data = new ArrayList<>();
     @Override
-
+    public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(ServerRequest.class, this::redirect)
                 .match(ServerList.class, this::saveServerList)
--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -62,7 +62,7 @@ public class ServerNode extends AllDirectives {
                 materializer
         );
         System.out.println(String.format("Server online at http://localhost:%d/\nPress RETURN to stop...", port));
-        System.in.read();
+
         binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 
