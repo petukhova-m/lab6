33a56d342fa57f8616bdeec3a656564392437422
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 21:41:49 +0300

    Scripted auto-commit on change (2021-12-17 21:41:49) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -13,7 +13,7 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.model.StatusCode;
 import akka.http.javadsl.model.Uri;
-
+import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.http.scaladsl.model.StatusCodes;
 import akka.japi.Pair;
@@ -33,7 +33,7 @@ import java.util.concurrent.ExecutionException;
 import static akka.pattern.PatternsCS.pipe;
 
 
-
+public class ServerNode extends AllDirectives {
     private static ActorRef config;
     private static Integer port;
 
