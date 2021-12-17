8da5da7502129c944738c2b5d13019d7c4979625
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 21:48:10 +0300

    Scripted auto-commit on change (2021-12-17 21:48:10) by gitwatch.sh

--- src/main/java/zkanonymizer/ConfigStorageActor.java
+++ src/main/java/zkanonymizer/ConfigStorageActor.java
@@ -8,12 +8,12 @@ import java.util.*;
 
     private ArrayList<String> data = new ArrayList<>();
     @Override
-
+    public Receive createReceive() {
 
                 .match(ServerRequest.class, this::redirect)
                 .match(ServerList.class, this::saveServerList)
                 .build();
-    }
+
 
     private void saveServerList(ServerList list) {
         data = new ArrayList<>(list.getData());
