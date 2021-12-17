cd1dd751e9c85daa2c183620570ffe1c2f902e94
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 22:26:19 +0300

    Scripted auto-commit on change (2021-12-17 22:26:19) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -30,7 +30,7 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 
-
+import static akka.pattern.PatternsCS.pipe;
 import static akka.actor.TypedActor.context;
 
 public class ServerNode extends AllDirectives {
