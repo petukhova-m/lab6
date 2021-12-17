409a05120dcc63bd38507d9ad10495ff3497db61
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 21:46:37 +0300

    Scripted auto-commit on change (2021-12-17 21:46:37) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -3,7 +3,7 @@ package zkanonymizer;
 import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import akka.actor.Props;
+
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
