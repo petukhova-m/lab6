c2ea96b2db12041fcc6ab78b9f866f07a845a80e
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 22:32:30 +0300

    Scripted auto-commit on change (2021-12-17 22:32:30) by gitwatch.sh

--- src/main/java/zkanonymizer/ConfigStorageActor.java
+++ src/main/java/zkanonymizer/ConfigStorageActor.java
@@ -12,11 +12,11 @@ public class ConfigStorageActor extends AbstractActor {
         return ReceiveBuilder.create()
                 .match(ServerRequest.class, this::redirect)
                 .match(ServerList.class, this::saveServerList)
-                .build();
+
     }
 
     private void saveServerList(ServerList list) {
-
+        data = new ArrayList<>(list.getData());
         System.out.println(data);
     }
 
