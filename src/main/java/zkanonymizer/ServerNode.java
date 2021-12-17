d9afb70950ab7c1268b29acc204a9be0ffbe39cf
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 23:02:58 +0300

    Scripted auto-commit on change (2021-12-17 23:02:58) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -26,7 +26,7 @@ import scala.concurrent.Future;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
+
 import java.util.concurrent.ExecutionException;
 
 
