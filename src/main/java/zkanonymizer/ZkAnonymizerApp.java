373e17466f1139985d18cfa67282f75124445926
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 22:07:04 +0300

    Scripted auto-commit on change (2021-12-17 22:07:04) by gitwatch.sh

--- src/main/java/zkanonymizer/ZkAnonymizerApp.java
+++ src/main/java/zkanonymizer/ZkAnonymizerApp.java
@@ -2,7 +2,7 @@
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.ZooDefs;
+
 import org.apache.zookeeper.ZooKeeper;
 import scala.Int;
 
