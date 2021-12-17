d65a3c20a74a7bb41a63b927b9c1c803d9cf9b0a
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 21:10:29 +0300

    Scripted auto-commit on change (2021-12-17 21:10:29) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerList.java
+++ src/main/java/zkanonymizer/ServerList.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 
 
-        return data;
+
     }
 
     public ServerList(List<String> data) {
--- src/main/java/zkanonymizer/ZkAnonymizerApp.java
+++ src/main/java/zkanonymizer/ZkAnonymizerApp.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
+
 
 
 public class ZkAnonymizerApp {
