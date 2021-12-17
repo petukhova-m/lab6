f6dae60e41826dd2a68caf949eb4c2276ccd42b9
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 20:15:14 +0300

    Scripted auto-commit on change (2021-12-17 20:15:14) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -88,7 +88,7 @@ public class ServerNode extends AllDirectives {
                                                             Integer.parseInt(
 
 
-
+                                                                                    config,
 
 
 
