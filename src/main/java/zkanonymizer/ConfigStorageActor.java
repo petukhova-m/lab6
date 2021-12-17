4f207b34594d221998a5d8ce42a7f887e28cbf74
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 22:03:19 +0300

    Scripted auto-commit on change (2021-12-17 22:03:19) by gitwatch.sh

--- src/main/java/zkanonymizer/ConfigStorageActor.java
+++ src/main/java/zkanonymizer/ConfigStorageActor.java
@@ -10,7 +10,7 @@ import java.util.*;
 
     public Receive createReceive() {
 
-
+                .match(ServerRequest.class, this::redirect)
                 .match(ServerList.class, this::saveServerList)
                 .build();
 
