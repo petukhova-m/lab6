f60f126cb1ef418294562a513af76b340be53413
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 21:22:39 +0300

    Scripted auto-commit on change (2021-12-17 21:22:39) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -73,7 +73,7 @@ import static akka.pattern.PatternsCS.pipe;
     }
 
     private Route get() {
-
+        return parameter("url", url ->
                     parameter("count", count -> {
                         int counter = Integer.parseInt(count);
                         final Http http = Http.get(system);
