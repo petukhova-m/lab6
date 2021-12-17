7c6c7ecdf3b8c25884ad43cfd255367ad2b1fe52
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 21:35:55 +0300

    Scripted auto-commit on change (2021-12-17 21:35:55) by gitwatch.sh

--- src/main/java/zkanonymizer/ZkWatcher.java
+++ src/main/java/zkanonymizer/ZkWatcher.java
@@ -1,7 +1,7 @@
 package zkanonymizer;
 
 import akka.actor.ActorRef;
-
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.Watcher.Event.*;
