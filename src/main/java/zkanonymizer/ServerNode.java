8fec8afc4632772bd2e17a18ae6652684256a685
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 21:00:19 +0300

    Scripted auto-commit on change (2021-12-17 21:00:19) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -74,7 +74,7 @@ public class ServerNode extends AllDirectives {
 
     private Route get() {
         return parameter("url", url ->
-
+                    parameter("count", count -> {
                         int counter = Integer.parseInt(count);
                         final Http http = Http.get(system);
                         if (counter == 0) {
