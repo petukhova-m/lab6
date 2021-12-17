83b580eaed6c57b9e8a7f085008d7aa14ee3cb0e
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 21:02:34 +0300

    Scripted auto-commit on change (2021-12-17 21:02:34) by gitwatch.sh

--- src/main/java/zkanonymizer/ServerNode.java
+++ src/main/java/zkanonymizer/ServerNode.java
@@ -36,7 +36,7 @@ import static akka.actor.TypedActor.context;
 public class ServerNode extends AllDirectives {
     private static ActorRef config;
     private static Integer port;
-    private static ActorSystem system;
+
     private static ZkWatcher watcher;
     public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         port = Integer.parseInt(args[0]);
